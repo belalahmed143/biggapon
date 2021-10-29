c895ba2c90c40a352baea5fb8c3372bfd3fe9e45
belalahmed143 <mohammadbelalahmed82@gmail.com>
2021-10-30 01:25:31 +0600

    heroku

--- biggaponbd/biggaponbd/settings.py
+++ biggaponbd/biggaponbd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'icz4uezn8jkq5v1)l%vr!fg*3%no$910&rb1ul)io2861%t#=7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'tolet-bangla.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1' , 'toletbangla.herokuapp.com']
 
 
 # Application definition
